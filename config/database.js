ec5a615a8b3c9dfc6263c0533c77a1a75f8cc325
Sai Ashwin <cuteram2008@gmail.com>
2016-05-29 12:31:07 +0530

    Just a few changes

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-	'url': 'mongodb://<user>:<pass>@jello.modulusmongo.net:27017/W5isuhux' //A test database, which you can use.
+	'url': 'mongodb://user:pass@jello.modulusmongo.net:27017/W5isuhux' //A test database, which you can use.
 };
